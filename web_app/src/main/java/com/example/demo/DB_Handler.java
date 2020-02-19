9fdd546b2e0b0954366139dd6d1d67a9ecf98f08
Gabe Joy <michael-joy@uiowa.edu>
2020-02-19 16:21:25 -0600

    added springboot app

---
+++ web_app/src/main/java/com/example/demo/DB_Handler.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import java.sql.*;
+
+public class DB_Handler {
+//    try{
+//        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/PostgreSQL 12/Senior_Design", "postgres", "postgres");
+//    }
+}
