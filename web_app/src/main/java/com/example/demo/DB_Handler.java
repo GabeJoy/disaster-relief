0f9627dc9c2e856d90d416aa42a33caad61ce095
Gabe Joy <michael-joy@uiowa.edu>
2020-03-07 21:54:32 -0600

    remade the springboot module, added a .gitignore

--- web_app/src/main/java/com/example/demo/DB_Handler.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import java.sql.*;
-
-public class DB_Handler {
-//    try{
-//        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/PostgreSQL 12/Senior_Design", "postgres", "postgres");
-//    }
-}
